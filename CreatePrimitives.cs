39f82bf5dc6996239f36a66b791c6e22a0f72bcb
sasazukov <zukovsasa231@gmail.com>
2025-11-30 17:24:54 +0300

    Add files via upload

--- CreatePrimitives.cs
+++ CreatePrimitives.cs
@@ -9,12 +9,12 @@ public class CreatePrimitives : MonoBehaviour
     //game object primitve
     void Start()
     {
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(3, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(2, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(1, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(1.5f , 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(2.5f , 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(2, 2, 0);
 
 
     }
